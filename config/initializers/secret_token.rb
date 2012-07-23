f88895a8c884d486a9c502035371ce19f5670ef3
Nick-Shortal <shortal.nick.j@edumail.vic.gov.au>
2012-07-23 13:34:27 +1000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'eb8e991ac209dc66dbdca3a5fcfcfb7a224c3c7d5e777fb791e10f0d7094fcbeb7d0293e01db38e191025fdc83d68a5e9e3304c3421f47bae2248b33dc68aa5c'
